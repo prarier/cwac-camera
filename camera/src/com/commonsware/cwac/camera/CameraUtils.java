0af9a4a144d214b87c59212a47fd9cba8c1c4a4f
Sergey Ivanov <Sergey.Ivanov@noveogroup.com>
2014-03-28 00:00:25 +0700

    CameraUtils: additional methods and techniques

--- camera/src/com/commonsware/cwac/camera/CameraUtils.java
+++ camera/src/com/commonsware/cwac/camera/CameraUtils.java
@@ -25,63 +25,30 @@ public class CameraUtils {
 
   private static final double ASPECT_TOLERANCE=0.1;
 
-  public static Camera.Size getOptimalPreviewSize(int displayOrientation,
-                                                  int width,
-                                                  int height,
-                                                  Camera.Parameters parameters) {
-    double targetRatio=(double)width / height;
-    List<Camera.Size> sizes=parameters.getSupportedPreviewSizes();
-    Camera.Size optimalSize=null;
-    double minDiff=Double.MAX_VALUE;
-    int targetHeight=height;
-
-    if (displayOrientation == 90 || displayOrientation == 270) {
-      targetRatio=(double)height / width;
-    }
-
-    // Try to find an size match aspect ratio and size
-
-    for (Size size : sizes) {
-      double ratio=(double)size.width / size.height;
-
-      if (Math.abs(ratio - targetRatio) <= ASPECT_TOLERANCE) {
-        if (Math.abs(size.height - targetHeight) < minDiff) {
-          optimalSize=size;
-          minDiff=Math.abs(size.height - targetHeight);
-        }
-      }
-    }
-
-    // Cannot find the one match the aspect ratio, ignore
-    // the requirement
-
-    if (optimalSize == null) {
-      minDiff=Double.MAX_VALUE;
-
-      for (Size size : sizes) {
-        if (Math.abs(size.height - targetHeight) < minDiff) {
-          optimalSize=size;
-          minDiff=Math.abs(size.height - targetHeight);
-        }
-      }
-    }
-
-    return(optimalSize);
+  public static Camera.Size getOptimalPreviewSize(
+      int displayOrientation, int width, int height, Camera.Parameters parameters
+  ) {
+    return getComparedSize(displayOrientation, width, height,
+        SizeComparator.MODE_PREVIEW, parameters.getSupportedPreviewSizes());
   }
 
-  public static Camera.Size getBestAspectPreviewSize(int displayOrientation,
-                                                     int width,
-                                                     int height,
-                                                     Camera.Parameters parameters) {
-    return(getBestAspectPreviewSize(displayOrientation, width, height,
-                                    parameters, 0.0d));
+  public static Camera.Size getLargestPictureSize(
+      int displayOrientation, int width, int height, Camera.Parameters parameters
+  ) {
+    return getComparedSize(displayOrientation, width, height,
+        SizeComparator.MODE_LARGEST, parameters.getSupportedPictureSizes());
   }
 
-  public static Camera.Size getBestAspectPreviewSize(int displayOrientation,
-                                                     int width,
-                                                     int height,
-                                                     Camera.Parameters parameters,
-                                                     double closeEnough) {
+  public static Camera.Size getClosestPictureSize(
+      int displayOrientation, int width, int height, Camera.Parameters parameters
+  ) {
+    return getComparedSize(displayOrientation, width, height,
+        SizeComparator.MODE_CLOSEST, parameters.getSupportedPictureSizes());
+  }
+
+  public static Camera.Size getBestAspectSize(
+      int displayOrientation, int width, int height, List<Camera.Size> sizes
+  ) {
     double targetRatio=(double)width / height;
     Camera.Size optimalSize=null;
     double minDiff=Double.MAX_VALUE;
@@ -90,10 +57,9 @@ public class CameraUtils {
       targetRatio=(double)height / width;
     }
 
-    List<Size> sizes=parameters.getSupportedPreviewSizes();
-
-    Collections.sort(sizes,
-                     Collections.reverseOrder(new SizeComparator()));
+    Collections.sort(sizes, Collections.reverseOrder(getComparator(
+        displayOrientation, width, height, SizeComparator.MODE_LARGEST
+    )));
 
     for (Size size : sizes) {
       double ratio=(double)size.width / size.height;
@@ -102,13 +68,21 @@ public class CameraUtils {
         optimalSize=size;
         minDiff=Math.abs(ratio - targetRatio);
       }
-
-      if (minDiff < closeEnough) {
-        break;
-      }
     }
 
-    return(optimalSize);
+    return optimalSize;
+  }
+
+  public static Camera.Size getBestAspectPreviewSize(
+      int displayOrientation, int width, int height, Camera.Parameters parameters
+  ) {
+    return getBestAspectSize(displayOrientation, width, height, parameters.getSupportedPreviewSizes());
+  }
+
+  public static Camera.Size getBestAspectPictureSize(
+      int displayOrientation, int width, int height, Camera.Parameters parameters
+  ) {
+    return getBestAspectSize(displayOrientation, width, height, parameters.getSupportedPictureSizes());
   }
 
   public static Camera.Size getLargestPictureSize(CameraHost host,
@@ -189,21 +163,115 @@ public class CameraUtils {
     return(match);
   }
 
-  private static class SizeComparator implements
-      Comparator<Camera.Size> {
-    @Override
-    public int compare(Size lhs, Size rhs) {
-      int left=lhs.width * lhs.height;
-      int right=rhs.width * rhs.height;
+  private static int calcArea(Size size) {
+    return size.width * size.height;
+  }
 
-      if (left < right) {
-        return(-1);
-      }
-      else if (left > right) {
-        return(1);
-      }
+  private static double calcRatio(Size size) {
+    return (double)size.width / size.height;
+  }
 
-      return(0);
+  private static double diffRatio(double ratio, Size size) {
+    return Math.abs(ratio - calcRatio(size));
+  }
+
+  private static class SizeComparator implements Comparator<Size> {
+    public static final int MODE_LARGEST = 1;
+    public static final int MODE_PREVIEW = 2;
+    public static final int MODE_CLOSEST = 3;
+
+    public final int mode;
+
+    public final int width;
+    public final int height;
+    public final double ratio;
+
+    public SizeComparator(int mode, int width, int height) {
+      this.mode = mode;
+      this.width = width;
+      this.height = height;
+      this.ratio = (double)width / height;
     }
+
+    @Override
+    public int compare(Size size1, Size size2) {
+      if (mode == MODE_LARGEST) return compareLargest(size1, size2);
+      if (mode == MODE_PREVIEW) return comparePreview(size1, size2);
+      if (mode == MODE_CLOSEST) return compareClosest(size1, size2);
+      return 0;
+    }
+
+    public int compareLargest(Size size1, Size size2) {
+      boolean fitRatio1 = diffRatio(ratio, size1) <= ASPECT_TOLERANCE;
+      boolean fitRatio2 = diffRatio(ratio, size2) <= ASPECT_TOLERANCE;
+
+      if (fitRatio1 && !fitRatio2) return -1;
+      if (fitRatio2 && !fitRatio1) return 1;
+
+      int area1 = calcArea(size1);
+      int area2 = calcArea(size2);
+
+      return area2 - area1;
+    }
+
+    public int compareFit(Size size1, Size size2) {
+      boolean fitSize1 = size1.width <= width && size1.height <= height;
+      boolean fitSize2 = size2.width <= width && size2.height <= height;
+
+      if (fitSize1 && !fitSize2) return -1;
+      if (fitSize2 && !fitSize1) return 1;
+
+      return 0;
+    }
+
+    public int comparePreview(Size size1, Size size2) {
+      int compareFit = compareFit(size1, size2);
+      if (compareFit != 0) return compareFit;
+
+      return compareLargest(size1, size2);
+    }
+
+    public int compareClosest(Size size1, Size size2) {
+      int diffWidth1 = Math.abs(width - size1.width);
+      int diffWidth2 = Math.abs(width - size2.width);
+      int diffHeight1 = Math.abs(height - size1.height);
+      int diffHeight2 = Math.abs(height - size2.height);
+
+      // Convoluted, but viable
+      int diffSize1 = (height * diffWidth1) + (width * diffHeight1);
+      int diffSize2 = (height * diffWidth2) + (width * diffHeight2);
+
+      return diffSize1 - diffSize2;
+    }
+  }
+
+  private static double calcRatio(int displayOrientation, int width, int height) {
+    double targetRatio = (double)width / height;
+    if (displayOrientation == 90 || displayOrientation == 270) {
+      targetRatio = (double)height / width;
+    }
+    return targetRatio;
+  }
+
+  @SuppressWarnings("SuspiciousNameCombination")
+  private static SizeComparator getComparator(
+      int displayOrientation, int width, int height, int mode
+  ) {
+    int normalizedWidth, normalizedHeight;
+    if (displayOrientation == 90 || displayOrientation == 270) {
+      normalizedWidth = height;
+      normalizedHeight = width;
+    } else {
+      normalizedWidth = width;
+      normalizedHeight = height;
+    }
+    return new SizeComparator(mode, normalizedWidth, normalizedHeight);
+  }
+
+  public static Camera.Size getComparedSize(
+      int displayOrientation, int width, int height, int mode, List<Camera.Size> sizes
+  ) {
+    Collections.sort(sizes, getComparator(displayOrientation, width, height, mode));
+    return sizes.get(0);
   }
 }
